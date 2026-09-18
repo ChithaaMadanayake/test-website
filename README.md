0c92bc8088abe550e50251b4258fa1dd116c5c66
Chithaa Madanayake <chithaa.20244018@iit.ac.lk>
2026-09-19 00:39:48 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# My Portfolio Website
+
+This is my personal portfolio website built with React.js, Tailwind CSS, and Three.js.
+
+It showcases my skills, projects, and a 3D animated laptop model.
+
+## Technologies Used
+- React.js
+- Three.js
+- Tailwind CSS
+
+## How to Run
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm run dev`
+4. Open `http://localhost:5173` in your browser
+
+## Live Website
+[https://chithaamadanayake.vercel.app/](https://chithaamadanayake.vercel.app/)
+
+## Contact
+Email: chithaa.20244018@iit.ac.lk
---
+++ src/assets/index.js
@@ -0,0 +1,99 @@
+import logo from "./logo.png";
+import ST_02 from "./ST_02.jpg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import gsap from './tech/gsap.png'
+import framer from './tech/framer.png';
+import css from "./tech/css.png";
+import python from "./python.png";
+import figma from "./tech/figma.png";
+import life from './life.jpg';
+import supra from './supra.jpg';
+import supa from './supa.png';
+import fire from './fire.png';
+import cinema from './cinema.png';
+import car from './car.jpg';
+import walletwatch from './walletwatch.jpg';
+import melomood from './melomood.jpg';
+import AX from './AX.png';
+import urbanOps from './urbanOps.jpg';
+import comment3 from './comment3.jpg';
+import comment4 from './comment4.jpg';
+import comment5 from './comment5.jpg';
+import comment1 from './comment1.jpeg';
+import csvdataanalyzer from './csvdataanalyzer.png';
+import comment2 from './comment2.jpeg';
+import portfolioImage from './portfolioImage.jpg';
+import comment6 from './comment6.png';
+import mysql from './tech/mysql.png';
+import express from './tech/express.png';
+import aws from './tech/aws.png';
+import mui from './tech/mui.png'
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import firstTestimonial from './Tars Ken.webp';
+import secondTestimonial from './image.webp';
+import thirdTestimonial from './third testimonial.webp';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  mysql,
+  express,
+  aws,
+  mui,
+  firstTestimonial,
+  secondTestimonial,
+  thirdTestimonial,
+  gsap,
+  framer,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  portfolioImage,
+  threejs,
+  walletwatch,
+  melomood,
+  life,
+  supra,
+  car,
+  cinema,
+  supa,
+  fire,
+  python,
+  comment3,
+  comment4,
+  comment5,
+  comment6,
+  comment1,
+  comment2,
+  csvdataanalyzer,
+  AX,
+  ST_02,
+  urbanOps,
+};
